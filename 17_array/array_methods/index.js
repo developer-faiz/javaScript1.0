d080c6cf4be6a4bafbf28361dc6f53a51b799485
developer-faiz <ahmadfaizanweb@gmail.com>
2023-07-27 20:56:28 +0530

    Array methods and String methods file added

--- 17_array/array_methods/index.js
+++ 17_array/array_methods/index.js
@@ -82,3 +82,28 @@ if(indexMonth != -1){
 }else{
     console.log('No such data found');
 }
+console.log('---------------------');
+const arr4 = [100,50,3,47,20,30];
+
+console.log(arr4);
+ arr4.sort((a,b)=>{
+    return b-a;
+});
+console.log(arr4)
+console.log('---------------------');
+console.log(arr4);
+console.log(arr4.reverse());
+
+console.log('---------------------');
+const arr6 = ['Faizan', 'Amit','This'];
+console.log(arr6);
+console.log(arr6.join('-'));
+let arr7 = 'This is about web';
+console.log(arr7)
+let temp = arr7.split();
+console.log(temp)
+const arr8 = [10,20,[20,'kl',[200,300,400,[1,3,5,7,11]]]];
+console.log(arr8)
+console.log(arr8.flat(2));
+console.log(arr8.flat(Infinity))
+
---
+++ 18_String/index.js
@@ -0,0 +1,47 @@
+// Creating String
+let a = 'Faizan';
+let b = new String('Ahmad');
+console.log(a + ' -------> ' + typeof a);
+console.log(b + ' --------> ' + typeof b);
+let c = new String('Faizan');
+console.log('---------------------');
+console.log( a == c);
+console.log(a === c);
+console.log('---------------------');
+let d = "This is 'about' javascript ";
+console.log(d);
+let e = 'Hi, developer welcome in "js"';
+console.log(e);
+console.log('---------------------');
+console.log('String Methods')
+console.log('---------------------');
+let str = 'Faizan Ahmad, i am Web developer ';
+console.log(str.length);
+console.log('---------------------');
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log('---------------------');
+console.log(str[3]);
+console.log(str.charAt(3));
+console.log(str.charAt(3));
+console.log(str.charCodeAt(7)) ;
+console.log(str.includes('Web'));
+console.log('---------------------');
+console.log(str);
+console.log(str.split());
+console.log('---------------------');
+console.log(str.indexOf('q',2));
+console.log(str.lastIndexOf('a',2));
+console.log('---------------------');
+// console.log(str)
+// let str1 = str.replace('e','a');
+// console.log(stra)
+let str3 = 'The rain in SPAIN stays mainly in the plain';
+let res = str3.match(/ain/gi);
+console.log(str3);
+console.log(res);
+console.log('---------------------');
+let r1 = str3.replace("a","e");
+console.log(str3);
+console.log(r1)
+console.log(str3.replaceAll("a","e"))
